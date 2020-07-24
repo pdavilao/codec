c6ccf70506bd433ce41b8e7e4fdb75623422e2c5
pdavilao <67982527+pdavilao@users.noreply.github.com>
2020-07-24 15:30:57 -0400

    Add files via upload

---
+++ operador_not.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=2;
+    if (!(a==2))
+    {
+        printf("Verdadero\n");
+    }
+    else
+    {
+        printf("Falso\n");
+    }
+    return 0;
+}
